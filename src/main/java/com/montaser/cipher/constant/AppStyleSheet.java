d49c1d6a5c4a8767c6db5ed62d38708784eacef8
Montaser Al-Aqrbawi <montaser.jjs@gmail.com>
2020-04-27 02:44:16 +0300

    Add red-jfx-tab-pane style to AppStyleSheet

--- src/main/java/com/montaser/cipher/constant/AppStyleSheet.java
+++ src/main/java/com/montaser/cipher/constant/AppStyleSheet.java
@@ -3,4 +3,6 @@ package com.montaser.cipher.constant;
 public final class AppStyleSheet {
 
     private static final String BASE_PATH = "/assist/css/";
+
+    public static final String RED_JFX_TAB_PANE = BASE_PATH.concat("red-jfx-tab-pane-style.css");
 }
