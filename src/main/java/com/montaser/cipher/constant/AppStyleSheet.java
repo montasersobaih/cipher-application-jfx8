cd4687b27e6307617663df498f22d8cbbaa50774
Montaser Al-Aqrbawi <montaser.jjs@gmail.com>
2020-04-28 12:08:29 +0300

    Add more style sheets:-     - red-jfx-button-style.css     - red-text-area-style.css
    Update AppStyleSheet class

--- src/main/java/com/montaser/cipher/constant/AppStyleSheet.java
+++ src/main/java/com/montaser/cipher/constant/AppStyleSheet.java
@@ -2,5 +2,10 @@ package com.montaser.cipher.constant;
 
 public final class AppStyleSheet {
 
+
+    public static final String RED_TEXT_AREA = ResourcePath.STYLE_SHEET.concat("red-text-area-style.css");
+
     public static final String RED_JFX_TAB_PANE = ResourcePath.STYLE_SHEET.concat("red-jfx-tab-pane-style.css");
+
+    public static final String RED_JFX_BUTTON = ResourcePath.STYLE_SHEET.concat("red-jfx-button-style.css");
 }
