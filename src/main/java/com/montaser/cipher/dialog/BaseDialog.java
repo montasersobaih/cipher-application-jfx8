34a2ec7d1bf24b72e98d64c02a0f3097abb29ac5
Montaser Al-Aqrbawi <montaser.jjs@gmail.com>
2020-04-30 02:12:50 +0300

    Modify BaseDialog class:-     - Make the return type parent 'Pane'

--- src/main/java/com/montaser/cipher/dialog/BaseDialog.java
+++ src/main/java/com/montaser/cipher/dialog/BaseDialog.java
@@ -2,10 +2,10 @@ package com.montaser.cipher.dialog;
 
 import com.jfoenix.controls.JFXDialog;
 import com.montaser.cipher.constant.enumeration.DialogTitle;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
-public abstract class BaseDialog<R extends Region> extends JFXDialog {
+public abstract class BaseDialog<R extends Pane> extends JFXDialog {
 
     public BaseDialog(StackPane pane, DialogTitle title) {
         super.setDialogContainer(pane);
